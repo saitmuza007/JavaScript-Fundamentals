33b353ed1ab53a7667b21111a16f2ac85dd5b370
saitmuza007 <muzz.sait@gmail.com>
2023-04-05 22:26:12 +0530

    First Class and Second Class

---
+++ 04-04-2023/index.js
@@ -0,0 +1,12 @@
+// var --- Globally Accessible Everywhere
+// let --- Is Fundamentally accessible only in the block 
+// const --- Is Accessisble only once for assignment 
+
+const names="Mathew"
+console.log(names) 
+
+{
+     names="Mathew Pravin"
+    console.log(names) 
+}
+console.log(names)
