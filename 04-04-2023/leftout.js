4a33b064bb12aded5452a6c39238ab807b83bd3d
saitmuza007 <muzz.sait@gmail.com>
2023-05-02 23:52:24 +0530

    missed

---
+++ 04-04-2023/leftout.js
@@ -0,0 +1,10 @@
+//numeric literals
+console.log(100_000_000+40000_000)
+console.log(1_100_00)
+//
+//Booleans
+let a=true
+let b="true"
+console.log(a==b)
+//
+console.log(0b011)
---
+++ 04-04-2023/prepostincrement.js
@@ -0,0 +1,8 @@
+var i=90
+console.log(i++)//90
+console.log(i)//91
+console.log(++i)//92
+console.log(i++ + ++i)
+console.log(2/3 )
+
+
