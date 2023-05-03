f0d018c00c00e81c92fd9c283c1ef5092b231b35
saitmuza007 <muzz.sait@gmail.com>
2023-05-03 22:07:22 +0530

    operators

---
+++ 04-04-2023/operators.js
@@ -0,0 +1,37 @@
+//Assignment Operator ....
+let a =20
+console.log(a)
+//Arithmetic Operator
+let b=10
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+//Remainder Operator
+console.log(a%b)
+//unary operators
+console.log(--a)
+console.log(a++)
+console.log(++a)
+
+//comparison operators
+console.log(a==b)
+console.log(a>=b)
+console.log(a>b)
+console.log(a<=b)
+console.log(a<b)
+
+//logical operators
+console.log(a!=b)
+console.log(a>b && b>a)
+console.log(a>b || b>a)
+//
+console.log(a||=b) 
+console.log(b)
+// nullish coalscing operator
+let c;
+let d;
+console.log(c??d)
+
+//Exponential operator
+console.log(a**b)
